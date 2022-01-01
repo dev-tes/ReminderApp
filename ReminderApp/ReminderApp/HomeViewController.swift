771693930479ba20c49a6ec9c6da2450c9c185c5
dev-tes <tesleem.amuda@gmail.com>
2022-01-01 20:09:05 +0100

    add right and left buttons of nav bar

--- ReminderApp/ReminderApp/HomeViewController.swift
+++ ReminderApp/ReminderApp/HomeViewController.swift
@@ -16,6 +16,24 @@ class HomeViewController: UIViewController {
         return table
     }()
 
+    let leftButton: UIButton = {
+        let skipButton = UIButton()
+        skipButton.setTitle("Test", for: .normal)
+        skipButton.setTitleColor(.black, for: .normal)
+        skipButton.translatesAutoresizingMaskIntoConstraints = false
+        skipButton.addTarget(self, action: #selector(didTapPlus), for: .touchUpInside)
+        skipButton.isHidden = false
+        return skipButton
+    }()
+    let rightButton: UIButton = {
+        let skipButton = UIButton()
+        skipButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        skipButton.tintColor = .black
+        skipButton.translatesAutoresizingMaskIntoConstraints = false
+        skipButton.addTarget(self, action: #selector(didTapTest), for: .touchUpInside)
+        skipButton.isHidden = false
+        return skipButton
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Reminder"
@@ -25,13 +43,41 @@ class HomeViewController: UIViewController {
         tableView.dataSource = self
         view.addSubview(tableView)
         tableView.frame = view.bounds
-        view.backgroundColor = .red
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
+
+        constraintViews()
     }
 
+    @objc func didTapTest(){
+        print("pressed")
+    }
+    @objc func didTapPlus(){
+        print("plus")
+    }
 
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func constraintViews(){
+
+//        view.addSubview(leftButton)
+//        view.addSubview(rightButton)
+//        self.navigationItem.titleView?.addSubview(leftButton)
+//        self.navigationItem.titleView?.addSubview(rightButton)
+
+        
+        NSLayoutConstraint.activate([
+
+//            leftButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+//            leftButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+
+//            rightButton.topAnchor.constraint(equalTo: view.topAnchor),
+//            rightButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
     }
@@ -41,6 +87,4 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
         cell.heightAnchor.constraint(equalToConstant: 100).isActive = true
         return cell
     }
-    
-    
 }
